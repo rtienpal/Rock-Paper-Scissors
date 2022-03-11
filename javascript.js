8bea627283122df26b4709fd959faf6e6b9dcdbd
Rafael Uen <rafaeluen@gmail.com>
2022-03-11 19:59:15 -0300

    Programmed for 5 rounds, all showing in console.log

--- javascript.js
+++ javascript.js
@@ -1,19 +1,74 @@
-let computerPlay = function()  {
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay()  {
     let randomNumber = Math.floor(Math.random()*100) + 1;
     return (randomNumber <= 33 ? "SCISSORS" :
             randomNumber <= 67 ? "PAPER" : "ROCK")
 }
 
-let playerPlay = function() {
-    let playerSelection = prompt("Choose ROCK, PAPER, or SCISSORS");
+function playerPlay(round) {
+    let playerSelection = prompt("Round "+round+"\nPlayer "+playerScore+" - "+computerScore+" Computer\nChoose ROCK, PAPER, or SCISSORS");
     if (playerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "SCISSORS") {
-        return playerSelection;
+        return playerSelection.toUpperCase();
     } else {
         console.log("Wrong value, please try again");
-        playerPlay();
+        return playerPlay(round);
     }
 }
 
-playerPlay();
+function win (playerSelection) {
+    playerScore++;
+    if (playerSelection === 'ROCK') {
+        return 'You win! Rock beats Scissors!';
+    } else if (playerSelection === 'SCISSORS') {
+        return 'You win! Scissors beats Paper!';
+    } else {
+        return 'You win! Paper beats Rock!';
+    }
+}
 
-console.log(computerPlay());
+function lose (playerSelection) {
+    computerScore++;
+    if (playerSelection === 'SCISSORS') {
+        return 'You lose! Rock beats Scissors!';
+    } else if (playerSelection === 'PAPER') {
+        return 'You lose! Scissors beats Paper!';
+    } else {
+        return 'You lose! Paper beats Rock!';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log('player selection = '+playerSelection+' computer selection = '+computerSelection);
+    if (playerSelection === computerSelection) {
+        return 'Tie! Both are '+playerSelection;
+    } else if (playerSelection === 'ROCK'){
+        if (computerSelection === 'SCISSORS') {
+            return win('ROCK');
+        } else {
+            return lose ('ROCK');
+        }
+    } else if (playerSelection === 'SCISSORS') {
+        if (computerSelection === 'PAPER') {
+            return win('SCISSORS');
+        } else {
+            return lose ('SCISSORS');
+        }
+    } else if (computerSelection === 'ROCK') {
+        return win('PAPER');
+    } else {
+        return lose('PAPER');
+    }
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = playerPlay(i);
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log('End of 5 rounds! Player '+playerScore+' x '+computerScore+' Computer');
+}
+
+game();
